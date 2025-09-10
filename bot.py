1c3ba1363d9a996392e36022b484a3b6fb511c31
temirbaevaulzhan2111-eng <temirbaevaulzhan2111@gmail.com>
2025-09-10 12:42:48 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,8 @@ def webhook():
 def index():
     return "✅ Бот работает!"
 
-if name == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
 
